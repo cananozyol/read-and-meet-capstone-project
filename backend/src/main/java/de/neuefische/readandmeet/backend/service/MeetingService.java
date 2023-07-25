065d633ba6cadeea4e424c097d57402e5a5fc385
cananozyol <canan.oezyol@live.de>
2023-07-25 22:50:04 +0200

    add UUID service for random id + new "add" method in service class

--- backend/src/main/java/de/neuefische/readandmeet/backend/service/MeetingService.java
+++ backend/src/main/java/de/neuefische/readandmeet/backend/service/MeetingService.java
@@ -1,6 +1,7 @@
 package de.neuefische.readandmeet.backend.service;
 
 import de.neuefische.readandmeet.backend.model.Meeting;
+import de.neuefische.readandmeet.backend.model.MeetingWithoutId;
 import de.neuefische.readandmeet.backend.repository.MeetingRepo;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,21 @@ public class MeetingService {
 
     private final MeetingRepo meetingRepo;
 
+    private final UuIdService uuIdService;
 
-    public MeetingService(MeetingRepo meetingRepo) {
+
+    public MeetingService(MeetingRepo meetingRepo, UuIdService uuIdService) {
         this.meetingRepo = meetingRepo;
+        this.uuIdService = uuIdService;
     }
 
     public List<Meeting> list() {
         return this.meetingRepo.findAll();
+    }
+
+    public Meeting add(MeetingWithoutId m) {
+        String id = uuIdService.getRandomId();
+        Meeting meeting = new Meeting(id, m.getTitle(), m.getDate(), m.getLocation());
+        return this.meetingRepo.insert(meeting);
     }
 }
