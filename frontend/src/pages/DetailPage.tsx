046c300c54fde0ceabaa1479e3e87aa70ebc2da7
cananozyol <canan.oezyol@live.de>
2023-07-26 23:09:48 +0200

    add detail page

---
+++ frontend/src/pages/DetailPage.tsx
@@ -0,0 +1,51 @@
+import {useNavigate, useParams} from "react-router-dom";
+import {Card, CardContent, Typography} from "@mui/material";
+import {useFetch} from "../hooks/useFetch.ts";
+import EventIcon from "@mui/icons-material/Event";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import {styled} from "styled-components";
+
+export default function DetailPage() {
+
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const { meetings } = useFetch();
+    const meeting = meetings.find((m) => m.id === id);
+
+
+    if (!meeting) {
+        return <>No Meeting</>;
+    }
+
+    return (
+        <Card sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <CardContent style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+                <Typography variant="h5" align="center">
+                    {meeting.title}
+                </Typography>
+                <Typography variant="body2" align="center" sx={{ display: "flex", alignItems: "center" }}>
+                    <EventIcon fontSize="small" sx={{ marginRight: "4px" }} />
+                    Date: {new Date(meeting.date).toLocaleDateString("de-DE")}
+                </Typography>
+                <Typography variant="body2" align="center" sx={{ display: "flex", alignItems: "center" }}>
+                    <LocationOnIcon fontSize="small" sx={{ marginRight: "4px" }} />
+                    Location: {meeting.location}
+                </Typography>
+            </CardContent>
+            <CardContent style={{ display: "flex", gap: "1rem", justifyContent: "center" }}>
+                <StyledButton>
+                    <button onClick={() => navigate(`/${id}/edit`)}> Edit </button>
+                    <button> Delete </button>
+                </StyledButton>
+            </CardContent>
+        </Card>
+    );
+}
+
+const StyledButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1.1em;
+  padding-top: 2em;
+`;
