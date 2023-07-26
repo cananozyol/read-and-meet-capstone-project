272e736ad2c94bf32f0923e69b312c30315fe35e
cananozyol <canan.oezyol@live.de>
2023-07-26 05:47:23 +0200

    change BackendApplicationTest

--- backend/src/test/java/de/neuefische/readandmeet/backend/BackendApplicationTest.java
+++ backend/src/test/java/de/neuefische/readandmeet/backend/BackendApplicationTest.java
@@ -2,12 +2,13 @@ package de.neuefische.readandmeet.backend;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BackendApplicationTests {
+
+class BackendApplicationTest {
+
     @Test
     void contextLoads() {
         Assertions.assertTrue(true);
     }
+
 }
