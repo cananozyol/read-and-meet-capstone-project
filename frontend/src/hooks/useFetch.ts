6db3503c50c18e00fe2496a8bcc0ba7442a8fbfc
cananozyol <canan.oezyol@live.de>
2023-07-26 23:07:54 +0200

    add useFetch for getById

--- frontend/src/hooks/useFetch.ts
+++ frontend/src/hooks/useFetch.ts
@@ -2,24 +2,23 @@ import {Meeting, MeetingWithoutId} from "../models/meeting.ts";
 import {create} from "zustand";
 import axios from "axios";
 
-
 type State = {
     meetings: Meeting[];
-    fetchMeetings: () => void,
+    fetchMeetings: () => void;
     postMeeting: (requestBody: MeetingWithoutId) => void;
+    getMeetingById: (id: string | undefined) => Meeting | undefined;
 };
 
 export const useFetch = create<State>((set, get) => ({
-
     meetings: [],
-
     fetchMeetings: () => {
         axios
             .get("api/meetings")
             .then((response) => response.data)
             .then((data) => {
-                set({meetings: data})
+                set({ meetings: data });
             })
+            .catch(console.error);
     },
 
     postMeeting: (requestBody: MeetingWithoutId) => {
@@ -30,4 +29,20 @@ export const useFetch = create<State>((set, get) => ({
             .catch(console.error);
     },
 
+    getMeetingById: (id: string | undefined) => {
+        if (!id) {
+            throw new Error("No id provided");
+        }
+        axios
+            .get(`/api/meetings/${id}`)
+            .then((response) => response.data)
+            .then((data) => {
+                set({ meetings: data });
+            })
+            .catch(console.error);
+        const { meetings } = get();
+        const meeting = meetings.find((meeting) => meeting.id === id);
+        return meeting;
+
+    },
 }));
