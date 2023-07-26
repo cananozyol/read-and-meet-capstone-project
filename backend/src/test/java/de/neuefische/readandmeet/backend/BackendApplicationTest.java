5d4d49968757d23255a93e1e3a20dbf7c4a9d1b3
cananozyol <canan.oezyol@live.de>
2023-07-26 17:55:54 +0200

    add empty lines

--- backend/src/test/java/de/neuefische/readandmeet/backend/BackendApplicationTest.java
+++ backend/src/test/java/de/neuefische/readandmeet/backend/BackendApplicationTest.java
@@ -10,5 +10,4 @@ class BackendApplicationTest {
     void contextLoads() {
         Assertions.assertTrue(true);
     }
-
 }
--- frontend/src/pages/MeetingList.tsx
+++ frontend/src/pages/MeetingList.tsx
@@ -26,5 +26,3 @@ export default function MeetingList(props: Props) {
         </>
     );
 }
-
-
