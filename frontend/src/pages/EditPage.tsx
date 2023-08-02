4d8aad16938b11d4499c3284e6bfefcd0aaf93cb
cananozyol <canan.oezyol@live.de>
2023-08-02 14:00:35 +0200

    add dialogcontent before cancel editing

--- frontend/src/pages/EditPage.tsx
+++ frontend/src/pages/EditPage.tsx
@@ -1,16 +1,17 @@
-import {useState} from "react";
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, TextField} from "@mui/material";
 import {useFetch} from "../hooks/useFetch.ts";
-import {Button, TextField} from "@mui/material";
 import {styled} from "styled-components";
 import {useNavigate, useParams} from "react-router-dom";
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
+import {useState} from "react";
 
 export default function EditPage() {
     const navigate = useNavigate();
     const { id } = useParams();
     const { putMeeting } = useFetch();
     const meeting = useFetch((state) => state.getMeetingById(id));
+    const [open, setOpen] = useState(false);
 
     const [formData, setFormData] = useState({
         title: meeting?.title || "",
@@ -30,6 +31,15 @@ export default function EditPage() {
     };
 
     const handleCancel = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleConfirmCancel = () => {
+        setOpen(false);
         navigate(`/${id}`);
     };
 
@@ -97,6 +107,22 @@ export default function EditPage() {
                     Save
                 </Button>
             </StyledButton>
+            <Dialog
+                open={open}
+                keepMounted
+                onClose={handleClose}
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description" sx={{ color: 'black' }}>
+                        Are you sure you want to cancel editing?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                    <Button onClick={handleClose} variant="outlined" sx={{ color: 'black', backgroundColor: '#d1adee' }}>No</Button>
+                    <Button onClick={handleConfirmCancel} variant="outlined" sx={{ color: 'black', backgroundColor: '#d1adee' }}>Yes</Button>
+                </DialogActions>
+            </Dialog>
         </form>
     );
 }
