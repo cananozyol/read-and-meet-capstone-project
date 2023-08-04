ab289c9200804209c3d1b8566a53815ed6a5ab44
cananozyol <canan.oezyol@live.de>
2023-08-04 22:40:33 +0200

    add book model and book list component

---
+++ frontend/src/pages/BookList.tsx
@@ -0,0 +1,30 @@
+import Grid from "@mui/material/Grid";
+import BookCard from "../components/BookCard.tsx";
+import {useBooks} from "../hooks/useBooks.ts";
+import {useEffect} from "react";
+import {Book} from "../models/books.ts";
+
+type Props = {
+    books: Book[];
+};
+
+export default function BookList(props: Props) {
+    const { books, fetchBooks } = useBooks();
+
+    useEffect(() => {
+        fetchBooks();
+    }, [fetchBooks]);
+
+    return (
+        <>
+            <p>You have <b>{books.length}</b> books</p>
+            <Grid container spacing={2}>
+                {props.books.map((book) => (
+                    <Grid key={book.id} item xs={6} sm={6} md={6} lg={6}>
+                        <BookCard book={book} key={book.id} />
+                    </Grid>
+                ))}
+            </Grid>
+        </>
+    );
+}
