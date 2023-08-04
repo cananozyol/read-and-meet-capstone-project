d478b9a0d9bfdba3403eae9285c7eeb10fde9c37
cananozyol <canan.oezyol@live.de>
2023-08-04 16:24:02 +0200

    update meeting model class

--- backend/src/main/java/de/neuefische/readandmeet/backend/model/Meeting.java
+++ backend/src/main/java/de/neuefische/readandmeet/backend/model/Meeting.java
@@ -2,20 +2,18 @@ package de.neuefische.readandmeet.backend.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "meetings")
 public class Meeting {
 
     @Id
-    private String id;
+    private final String id;
     private String title;
     private LocalDate date;
     private String location;
