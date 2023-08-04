32c7ef6717a0d1e242ac432bfde82c3590a0652d
cananozyol <canan.oezyol@live.de>
2023-08-04 22:40:17 +0200

    change app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,27 +4,30 @@ import MeetingList from "./pages/MeetingList.tsx";
 import AddPage from "./pages/AddPage.tsx";
 import DetailPage from "./pages/DetailPage.tsx";
 import EditPage from "./pages/EditPage.tsx";
-import {useFetch} from "./hooks/useFetch.ts";
+import BookList from "./pages/BookList.tsx"; // Import the BookList component
+import {useMeetings} from "./hooks/useMeetings.ts";
 import {styled} from "styled-components";
 import HomePage from "./pages/HomePage.tsx";
 import {Button} from "@mui/material";
 import {ToastContainer} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
+import "react-toastify/dist/ReactToastify.css";
+import {useBooks} from "./hooks/useBooks.ts";
 
 export default function App() {
-    const meetings = useFetch((state) => state.meetings);
+    const meetings = useMeetings((state) => state.meetings);
+    const books = useBooks((state) => state.books);
 
     return (
         <main>
             <Header />
-            <ToastContainer/>
+            <ToastContainer />
             <Routes>
-                <Route path="/" element={<HomePage/>}/>
+                <Route path="/" element={<HomePage />} />
                 <Route path="/meetinglist" element={<MeetingList meetings={meetings} />} />
                 <Route path="/addmeeting" element={<AddPage />} />
                 <Route path="/:id" element={<DetailPage />} />
-                <Route path="/:id/editmeeting" element={<EditPage/>} />
+                <Route path="/:id/editmeeting" element={<EditPage />} />
+                <Route path="/booklist" element={<BookList books={books}/>} />
             </Routes>
 
             <StyledApp>
@@ -33,10 +36,10 @@ export default function App() {
                         variant="contained"
                         color="secondary"
                         sx={{
-                            backgroundColor: '#d1adee',
-                            color: 'black',
-                            borderRadius: '5px',
-                            width: '139px',
+                            backgroundColor: "#d1adee",
+                            color: "black",
+                            borderRadius: "5px",
+                            width: "139px",
                         }}
                     >
                         MY MEETINGS
@@ -47,13 +50,27 @@ export default function App() {
                         variant="contained"
                         color="secondary"
                         sx={{
-                            backgroundColor: '#d1adee',
-                            color: 'black',
-                            borderRadius: '5px',
-                            width: '139px',
+                            backgroundColor: "#d1adee",
+                            color: "black",
+                            borderRadius: "5px",
+                            width: "139px",
                         }}
                     >
                         NEW MEETING
+                    </Button>
+                </Link>
+                <Link to={"/booklist"}>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        sx={{
+                            backgroundColor: "#d1adee",
+                            color: "black",
+                            borderRadius: "5px",
+                            width: "139px",
+                        }}
+                    >
+                        MY BOOKS
                     </Button>
                 </Link>
             </StyledApp>
