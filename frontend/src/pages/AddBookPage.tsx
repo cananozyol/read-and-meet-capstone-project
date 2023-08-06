579a592af53c01ef8100cce728a0156763fc3bc4
cananozyol <canan.oezyol@live.de>
2023-08-06 15:39:38 +0200

    add AddBookPage for adding new books

---
+++ frontend/src/pages/AddBookPage.tsx
@@ -0,0 +1,145 @@
+import {useBooks} from "../hooks/useBooks.ts";
+import {ChangeEvent, FormEvent, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {Genre, Status} from "../models/books.ts";
+
+export default function AddBookPage() {
+
+    const navigate = useNavigate();
+    const { postBook } = useBooks();
+
+    const [formData, setFormData] = useState({
+        title: "",
+        author: "",
+        genre: Genre.NOT_SELECTED,
+        status: Status.NOT_READ,
+        rating: 0,
+    });
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
+    };
+
+    const handleGenreChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        const genre = event.target.value as Genre;
+        setFormData((prevData) => ({ ...prevData, genre }));
+    };
+
+    const handleStatusChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const status = event.target.value as Status;
+        setFormData((prevData) => ({ ...prevData, status }));
+    };
+
+    const handleRatingChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const rating = parseInt(event.target.value);
+        setFormData((prevData) => ({ ...prevData, rating }));
+    };
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        postBook(formData);
+        navigate("/booklist");
+    };
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Add Book</h1>
+            <label htmlFor="book-title">Title</label>
+            <input
+                type="text"
+                id="book-title"
+                name="title"
+                value={formData.title}
+                onChange={handleInputChange}
+                required
+            />
+
+            <label htmlFor="book-author">Author</label>
+            <input
+                type="text"
+                id="book-author"
+                name="author"
+                value={formData.author}
+                onChange={handleInputChange}
+                required
+            />
+
+            <label htmlFor="book-genre">Genre</label>
+            <select
+                id="book-genre"
+                name="genre"
+                value={formData.genre}
+                onChange={handleGenreChange}
+                required
+            >
+                <option value={Genre.NOT_SELECTED}>Select</option>
+                <option value={Genre.CLASSIC}>Classic</option>
+                <option value={Genre.FANTASY}>Fantasy</option>
+                <option value={Genre.MYSTERY}>Mystery</option>
+                <option value={Genre.ROMANCE}>Romance</option>
+                <option value={Genre.SCIENCE_FICTION}>Science Fiction</option>
+                <option value={Genre.THRILLER}>Thriller</option>
+                <option value={Genre.HORROR}>Horror</option>
+                <option value={Genre.NON_FICTION}>Non-Fiction</option>
+            </select>
+
+            <label>Status</label>
+            <div>
+                <label>
+                    <input
+                        type="radio"
+                        name="status"
+                        value={Status.NOT_READ}
+                        checked={formData.status === Status.NOT_READ}
+                        onChange={handleStatusChange}
+                    />
+                    Not Read
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="status"
+                        value={Status.READING}
+                        checked={formData.status === Status.READING}
+                        onChange={handleStatusChange}
+                    />
+                    Reading
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="status"
+                        value={Status.READ}
+                        checked={formData.status === Status.READ}
+                        onChange={handleStatusChange}
+                    />
+                    Read
+                </label>
+            </div>
+
+            <label>Rating</label>
+            <div>
+                {[0, 1, 2, 3, 4, 5].map((rating) => (
+                    <label key={rating}>
+                        <input
+                            type="radio"
+                            name="rating"
+                            value={rating}
+                            checked={formData.rating === rating}
+                            onChange={handleRatingChange}
+                        />
+                        {rating}
+                    </label>
+                ))}
+            </div>
+
+            <div>
+                <button type={"submit"}>Add</button>
+                <button onClick={() => navigate('/booklist')}>Cancel</button>
+            </div>
+        </form>
+    )
+}
+
