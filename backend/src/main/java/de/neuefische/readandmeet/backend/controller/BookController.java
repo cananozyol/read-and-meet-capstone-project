294da7c5b3e53b0077e00be1e109b4daf03a0ce4
cananozyol <canan.oezyol@live.de>
2023-08-08 15:42:03 +0200

    empty lines

--- backend/src/main/java/de/neuefische/readandmeet/backend/controller/BookController.java
+++ backend/src/main/java/de/neuefische/readandmeet/backend/controller/BookController.java
@@ -60,5 +60,4 @@ public class BookController {
             return ResponseEntity.notFound().build();
         }
     }
-
 }
--- frontend/src/models/books.ts
+++ frontend/src/models/books.ts
@@ -10,13 +10,15 @@ export type Book = {
 export enum Genre {
     NOT_SELECTED = "NOT_SELECTED",
     CLASSIC = "CLASSIC",
-    FANTASY = "FANTASY",
-    MYSTERY = "MYSTERY",
+    DRAMA = "DRAMA",
     ROMANCE = "ROMANCE",
+    FANTASY = "FANTASY",
     SCIENCE_FICTION = "SCIENCE_FICTION",
     THRILLER = "THRILLER",
+    BIOGRAPHY = "BIOGRAPHY",
     HORROR = "HORROR",
     NON_FICTION = "NON_FICTION",
+    MYSTERY = "MYSTERY"
 }
 
 export enum Status {
