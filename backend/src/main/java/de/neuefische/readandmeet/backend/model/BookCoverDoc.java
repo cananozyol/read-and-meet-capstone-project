5f118dd1cc907eb703449d6b174d0891237c15b0
cananozyol <canan.oezyol@live.de>
2023-08-15 21:17:29 +0200

    update backend webclient response class

--- backend/src/main/java/de/neuefische/readandmeet/backend/model/BookCoverDoc.java
+++ backend/src/main/java/de/neuefische/readandmeet/backend/model/BookCoverDoc.java
@@ -1,11 +1,8 @@
 package de.neuefische.readandmeet.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
-@AllArgsConstructor
+
 public class BookCoverDoc {
 
     @JsonProperty("cover_i")
--- backend/src/main/java/de/neuefische/readandmeet/backend/model/OpenLibrarySearchResponse.java
+++ backend/src/main/java/de/neuefische/readandmeet/backend/model/OpenLibrarySearchResponse.java
@@ -1,12 +1,9 @@
 package de.neuefische.readandmeet.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.util.List;
-@Data
-@AllArgsConstructor
+
 public class OpenLibrarySearchResponse {
 
     @JsonProperty("docs")
